2f2eced488c624c5b845c861b91d513cc9542bff
Lynsey Teague <lynsey.teague@yahoo.com>
2016-03-21 20:51:59 -0400

    functionality revised

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 angular.module('SameDomainRequest', [])
     .controller('myController', function ($scope, $http) {
-
+        $scope.results = [];
+        $scope.isSearching = false;
+        $scope.totalNumResults = true;
         $scope.getJSONData = function () {
             $scope.isSearching = true;
             $http({
@@ -9,13 +11,16 @@ angular.module('SameDomainRequest', [])
                 params: {
                     method: 'flickr.photos.search',
                     api_key: 'e12e5a3fcefd432c67a2d131c32afafc',
-                    text: 'cat',
+                    text: $scope.searchTerm,
                     format: 'json',
                     nojsoncallback: 1
                 }
             }).success(function (data) {
                 $scope.results = data;
                 $scope.isSearching = false;
+                $scope.totalNumResults = false;
+                $scope.searchTerm = '';
+                console.log(data.photos.total);
             }).error(function (error) {
                 console.error(error);
                 $scope.isSearching = true;
